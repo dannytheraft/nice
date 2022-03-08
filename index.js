04dd2a9397ee3bb109e1ec25189a7fd5b96a2085
TheNewGuyDanny <96438665+dannytheraft@users.noreply.github.com>
2022-03-08 23:15:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const puppeteer = require('puppeteer');
 const express = require('express');
 const app = express();
-const { test } = require('playwright');
 
 /*app.get('/', async(req,res) => {
 const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
@@ -12,11 +11,11 @@ const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox',
   
   res.send({ data: await page.content() })
 })*/
-app.get('/', async (req,res) => {
+/*app.get('/', async (req,res) => {
 test('Page Screenshot', async ({ page }) => {
     await page.goto('https://discord.com/');
       res.send({ data: await page.content() })
 });
-})
-//app.get('/', async(req,res) =>res.send('nobb'))
+})*/
+app.get('/', async(req,res) =>res.send('nobb'))
 app.listen(3000);
